b2c6f2997474d891852005a6470a533c1d4985d7
Beasta <Beasta@users.noreply.github.com>
2015-09-22 15:41:12 -0700

    Properly incrementing for offset start of data. Working on getting Heroku setup with a Procfile. Factored out styling into seperate CSS. Also working on displaying more than a week worth of data.

--- server/myServer/public/heatmap.js
+++ server/myServer/public/heatmap.js
@@ -13,12 +13,15 @@ var hourlyAverage = function(data){
   var maxValue=0;
   var minValue=200;
   var theAverage = 60;
+  //grab a weeks worth of datapoints - 10080 minutes in a week, or however many points are in data.length if its shorter than 10080
+  var dataPointsToGrab = data.length > 10080 ? 10080 : data.length;
+  console.log('dataPointsToGrab',dataPointsToGrab);
 
-  for (var i = 0; i < data.length; i++){
+  for (var i = 0; i < dataPointsToGrab; i++){
     theDate = new Date(data[i].date);
     theDay = theDate.getDay();
     theHour = theDate.getHours();
-    // theMinute = theDate.getMinutes();
+    theWeek = d3.time.weekOfYear(theDate);
 
     if ( thisHour === theHour ) { // we haven't rolled over a new hour, if item is valid, increment hours items, add to hourssum
 
@@ -40,8 +43,9 @@ var hourlyAverage = function(data){
       }
       //add a data point for the hour
       hourlyArr.push ( { //recreating data structure of previous example
-        day: theDay+1,
-        hour: theHour+1,
+        day: theDay,
+        hour: theHour,
+        week: theWeek,
         value: theAverage,
         oldDate: theDate
       } ) ;
@@ -64,93 +68,118 @@ var margin = { top: 50, right: 0, bottom: 100, left: 30 },
           gridSize = Math.floor(width / 24),
           legendElementWidth = gridSize*2,
           buckets = 9,
-          colors = ["#ffffd9","#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#253494","#081d58"], // alternatively colorbrewer.YlGnBu[9]
-          days = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"],
+          // colors = ["#ffffd9","#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#2ppasdfasdfasdf53494","#081d58"], // alternatively colorbrewer.YlGnBu[9]
+          // colors = ['#0001FF','#571BE8','#6E0FE8','#E41DFF', '#E80F67', '#E80F2D','#FF2C10'];
+          colors = [
+            '#009933',
+            '#208C46',
+            '#408059',
+            '#60736C',
+            '#806680',
+            '#9F5993',
+            '#BF4DA6',
+            '#DF40B9',
+            '#FF33CC'
+          ],
+          days = [ "Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"],
           times = ["1a", "2a", "3a", "4a", "5a", "6a", "7a", "8a", "9a", "10a", "11a", "12a", "1p", "2p", "3p", "4p", "5p", "6p", "7p", "8p", "9p", "10p", "11p", "12p"];
 
 
-      // d3.tsv("data.  tsv",
-      //   function(d) {
-      //     return {
-      //       day: +d.day,
-      //       hour: +d.hour,
-      //       value: +d.value
-      //     };
-      //   },
-    var createMap = function( data, min, max ) {
-          // var weeksInData = d3.select("#weekBox")
-          //     .data( d3.set( d3.time.weekOfYear( data.oldDate ).values( ) ) )
-          //     .append("option")
-          //     .attr('id','32')
-          //     .attr('class','weeks')
-          //     .text("text","weekbox");
+  // d3.tsv("data.  tsv",
+  //   function(d) {
+  //     return {
+  //       day: +d.day,
+  //       hour: +d.hour,
+  //       value: +d.value
+  //     };
+  //   },
+var createMap = function( data, min, max ) {
+  // var weeksInData = d3.select("#weekBox")
+  //     .data( d3.set( d3.time.weekOfYear( data.oldDate ).values( ) ) )
+  //     .append("option")
+  //     .attr('id','32')
+  //     .attr('class','weeks')
+  //     .text("text","weekbox");
 
-          var colorScale = d3.scale.quantile()
-              .domain([min, d3.max(data, function (d) { return d.value; })])
-              .range(colors);
+  console.log('creatingMap');
 
-          var svg = d3.select("#chart").append("svg")
-              .attr("width", width + margin.left + margin.right)
-              .attr("height", height + margin.top + margin.bottom)
-              .attr("style", "outline: none")
-              .append("g")
-              .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  var colorScale = d3.scale.quantile()
+      .domain([min, d3.max(data, function (d) { return d.value; })])
+      .range(colors);
 
-          var dayLabels = svg.selectAll(".dayLabel")
-              .data(days)
-              .enter().append("text")
-                .text(function (d) { return d; })
-                .attr("x", 0)
-                .attr("y", function (d, i) { return i * gridSize; })
-                .style("text-anchor", "end")
-                .attr("transform", "translate(-6," + gridSize / 1.5 + ")")
-                .attr("class", function (d, i) { return ((i >= 0 && i <= 4) ? "dayLabel mono axis axis-workweek" : "dayLabel mono axis"); });
+  var svg = d3.select("#chart").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .attr("style", "outline: none")
+      .append("g") //
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-          var timeLabels = svg.selectAll(".timeLabel")
-              .data(times)
-              .enter().append("text")
-                .text(function(d) { return d; })
-                .attr("x", function(d, i) { return i * gridSize; })
-                .attr("y", 0)
-                .style("text-anchor", "middle")
-                .attr("transform", "translate(" + gridSize / 2 + ", -6)")
-                .attr("class", function(d, i) { return ((i >= 7 && i <= 16) ? "timeLabel mono axis axis-worktime" : "timeLabel mono axis"); });
+  var dayLabels = svg.selectAll(".dayLabel")
+      .data(days)
+      .enter().append("text")
+        .text(function (d) { return d; })
+        .attr("x", 0)
+        .attr("y", function (d, i) { return i * gridSize; })
+        .style("text-anchor", "end")
+        .attr("transform", "translate(-6," + gridSize / 1.5 + ")")
+        .attr("class", function (d, i) { return ((i >= 0 && i <= 4) ? "dayLabel mono axis axis-workweek" : "dayLabel mono axis"); });
 
-          var heatMap = svg.selectAll(".hour")
-              .data(data)
-              .enter().append("rect")
-              .attr("x", function(d) { return (d.hour - 1) * gridSize; })
-              // .attr("y", function(d) { return (d.day - 1) * gridSize; })
-              .attr("y", function(d) { return (d3.time.weekOfYear(d.oldDate) % 7 ) * gridSize; })
-              .attr("rx", 0)
-              .attr("ry", 0)
-              .attr("class", "hour ")
-              .attr("width", gridSize)
-              .attr("height", gridSize)
-              .style("fill", function(d) {
-                // return colors[Math.floor(Math.random()*9)];
-                return colors[9];
-              });
+  var timeLabels = svg.selectAll(".timeLabel")
+      .data(times)
+      .enter().append("text")
+        .text(function(d) { return d; })
+        .attr("x", function(d, i) { return i * gridSize; })
+        .attr("y", 0)
+        .style("text-anchor", "middle")
+        .attr("transform", "translate(" + gridSize / 2 + ", -6)")
+        .attr("class", function(d, i) { return ((i >= 7 && i <= 16) ? "timeLabel mono axis axis-worktime" : "timeLabel mono axis"); });
 
-          heatMap.transition().duration( 10000 * Math.random() )
-              .style("fill", function(d) { return colorScale(d.value); });
+  var heatMap = svg.selectAll(".hour")
+      .data(data)
+      .enter().append("rect")
+        .attr("x", function(d) { return ( d.hour ) * gridSize; })
+        .attr("y", function(d) { 
+          // console.log('theday',( d.week - data[0].week) * 7 + d.day , 'data[0].week', data[0].week);
+          return ( (  d.week - data[0].week ) * 7 + d.day ) * gridSize; })
+        // .attr("y", function(d) { return (d3.time.weekOfYear(d.oldDate) % 7 ) * gridSize; })
+        .attr("rx", 0)
+        .attr("ry", 0)
+        .attr("class", "hour ")
+        .attr("width", gridSize)
+        .attr("height", gridSize)
+        .style("fill", function(d) {
+          // return colors[Math.floor(Math.random()*9)];
+          return colors[9];
+        });
 
-          // heatMap.append("title").text(function(d) { return d.value; });  // this doesn't appear to be functional
-          var legend = svg.selectAll(".legend")
-              .data([0].concat(colorScale.quantiles()), function(d) { return d; })
-              .enter().append("g")
-              .attr("class", "legend");
+  heatMap.transition().duration( 3000 )
+      .style("fill", function(d) { return colorScale(d.value); });
 
-          legend.append("rect")
-            .attr("x", function(d, i) { return legendElementWidth * i; })
-            .attr("y", height)
-            .attr("width", legendElementWidth)
-            .attr("height", gridSize / 2)
-            .style("fill", function(d, i) { return colors[i]; });
+  // heatMap.append("title").text(function(d) { return d.value; });  // this doesn't appear to be functional
 
-          legend.append("text")
-            .attr("class", "mono")
-            .text(function(d) { return "≥ " + Math.round(d); })
-            .attr("x", function(d, i) { return legendElementWidth * i; })
-            .attr("y", height + gridSize);
-      };
+  //create legend svg
+  var legend = svg.selectAll(".legend")
+      .data([0].concat(colorScale.quantiles()), function(d) { return d; })
+      .enter().append("g")
+        .attr("class", "legend");
+
+  legend.append("rect")
+    .attr("x", function(d, i) { return legendElementWidth * i; })
+    .attr("y", height)
+    .attr("width", legendElementWidth)
+    .attr("height", gridSize / 2)
+    .style("fill", function(d, i) { return colors[i]; });
+
+  legend.append("text")
+    .attr("class", "mono")
+    .text(function(d) { return "≥ " + Math.round(d); })
+    .attr("x", function(d, i) { return legendElementWidth * i; })
+    .attr("y", height + gridSize);
+};
+
+//The g element is a container used to group objects. Transformations applied to the g element are performed on 
+//all of its child elements. Attributes applied are inherited by child elements. In addition, it can be used to 
+//define complex objects that can later be referenced with the <use> element.
+
+
+
